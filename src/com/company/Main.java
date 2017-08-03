8c762dabf6ce1d3bdbce214d8baf45516a0cb99f
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2017-08-03 08:04:16 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        try {
+            //testInput(-1);
+            //testInput(100);
+            Random random = new Random();
+            float f = 4.5f;
+            int test = (int)f;
+            byte[] bytes=null;
+            random.nextBytes(bytes);
+        }
+        catch (IndexOutOfBoundsException ex){
+            System.out.println("There was an error in the program: " + ex.getMessage());
+            System.out.println("The correct inputs for the program are: 1 to 10000\nExample below would print squares upto 10");
+            testInput(10);
+        }
+    }
+
+    static void testInput(int number){
+
+        if(number<=0)
+            throw new IndexOutOfBoundsException("Number cannot be zero or less");
+        if(number>10000)
+            throw new StackOverflowError("This number cannot be greater than 10000");
+        for (int i=2; i<number; i+=2){
+            System.out.println(i*i);
+        }
+    }
+}
